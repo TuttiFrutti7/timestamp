5b329ddc268d120ced308ed599add361dabc9536
tuttifrutti7 <daces777@gmail.com>
2025-06-05 13:50:51 +0300

    Implemented User seeder and factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->unique()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'auth_method' => 'email',
+            'role' => 'user',
+            'hidden_profile' => false,
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
